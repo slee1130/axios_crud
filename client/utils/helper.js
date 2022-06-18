9d59e69f07a5bd0fc7dead90b8eb6b594b6043ac
Alice Lee <alicesmlee91@gmail.com>
2022-06-18 10:09:47 +0900

    fix: remove

--- client/utils/helper.js
+++
@@ -1,6 +0,0 @@
-export const sum = (a, b) => a + b;
-export const divide = (a, b) => a / b;
-export const getCookie = (name) => {
-  var match = document.cookie.match(new RegExp("(^| )" + name + "=([^;]+)"));
-  if (match) return match[2];
-};
